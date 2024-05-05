bb031392f9b55e9585b9f17865c55393632c38cd
sauravxcode <sauravchaturvedi1234@gmail.com>
2024-05-06 00:25:48 +0530

    First Commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero';
+import PopularProducts from './PopularProducts';
+import Footer from './Footer';
+import CustomerReviews from './CustomerReviews';
+import Services from './Services';
+import SpecialOffer from './SpecialOffer';
+import SuperQuality from './SuperQuality';
+import Subscribe from './Subscribe';
+
+export {
+    Hero,
+    PopularProducts,
+    Footer,
+    CustomerReviews,
+    Services,
+    SpecialOffer,
+    SuperQuality,
+    Subscribe,
+}
